b6851219c21d374a232dea3daf027116bf420aba
ritikkr066 <ritikkrsiwan345@gmail.com>
2024-04-06 19:20:56 +0530

    register

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,33 @@ export const metadata={
 }
 export default function Home() {
   return (
-    <main className="pt-10">
+    <main className="">
         <div className="max-sm:block max-md:hidden md:hidden">
-          sm
+          <div className="w-full h-[90vh]">
+            <Image width={300} height={900} className="absolute -z-2 w-full h-[90vh] brightness-50" src="/doctors.jpg" priority alt="blank"></Image>
+            <div className="absolute flex  w-full h-[90vh] justify-center items-center">
+              <div className="flex flex-col ml-6">
+                <span className="mt-3 text-sm font-bold text-white">we are here for your care</span>
+                <span className="text-5xl font-bold text-red-500 mt-">Best Care &</span>
+                <span className="text-5xl font-bold text-red-500">Better Doctor</span>
+                <p className=" font-[6px] text-white">
+                The first medical degrees were awarded by the Schola Medica Salernitana around the year 1000, including to women such as Trota of Salerno.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="max-sm:hidden max-md:block md:block">
-          md
+        <div className="pt-24 max-sm:hidden max-md:block md:block">
+          <div className="flex justify-between w-full">
+            <div className="w-[40vw]">
+
+            </div>
+            <div className="w-[60vw] flex justify-center">
+            <Image width={300} height={900} className="p-2 w-[90%] lg:w-[500px] h-[60vh]" src="/doctors.jpg" priority alt="blank"></Image>
+            </div>
+          </div>
         </div>
     </main>
   );
 }
+
--- src/middleware.js
+++ src/middleware.js
@@ -1,42 +1,42 @@
-// export default function middleware(){
-//     console.log("middleware");
+ export default function middleware(){
+   console.log("middleware");
+ }
+
+// import { NextResponse } from 'next/server'
+ 
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request) {
+//     console.log("middleware used")
+// //   return NextResponse.redirect(new URL('/login', request.url));
+//   const authToken= request.cookies.get("authtoken")?.value;
+
+//   if(request.nextUrl.pathname==="/api/login"){
+//     return;
+//   }
+
+//   const loggedInUserNotAccessPaths=request.nextUrl.pathname==="/login" || request.nextUrl.pathname=="/register";
+
+//   if(loggedInUserNotAccessPaths){
+//     //accessing not secured route
+//     if(authToken){
+//          return NextResponse.redirect(new URL("/user-profile",request.url));
+//     }else{
+//         //accessing secured  route
+//         if(!authToken){
+//             return NextResponse.redirect(new URL('/login', request.url));
+//         }else{
+//             //verify ...
+//         }
+//     }
+//   }
+// console.log(authToken)
 // }
-
-import { NextResponse } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
-export function middleware(request) {
-    console.log("middleware used")
-//   return NextResponse.redirect(new URL('/login', request.url));
-  const authToken= request.cookies.get("authtoken")?.value;
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: ['/user-profile',
+//    "/Appointment",
+//     "/api/:path*",
 
-  if(request.nextUrl.pathname==="/api/login"){
-    return;
-  }
-
-  const loggedInUserNotAccessPaths=request.nextUrl.pathname==="/login" || request.nextUrl.pathname=="/register";
-
-  if(loggedInUserNotAccessPaths){
-    //accessing not secured route
-    if(authToken){
-         return NextResponse.redirect(new URL("/user-profile",request.url));
-    }else{
-        //accessing secured  route
-        if(!authToken){
-            return NextResponse.redirect(new URL('/login', request.url));
-        }else{
-            //verify ...
-        }
-    }
-  }
-console.log(authToken)
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ['/user-profile',
-   "/Appointment",
-    "/api/:path*",
-
-]
-}
+// ]
+// }
