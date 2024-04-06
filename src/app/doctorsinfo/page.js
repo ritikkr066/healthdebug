ff37e4279af81e8622642314ffe2af81c97d6660
ritikkr066 <ritikkrsiwan345@gmail.com>
2024-04-07 00:49:39 +0530

    doctorsinfo

--- src/app/doctorsinfo/page.js
+++ src/app/doctorsinfo/page.js
@@ -21,10 +21,10 @@
 import Card from "../components/Card"
 async function itemList(){
     try {
-        const response=await fetch("http://localhost:3000/api/doctor1");
+        // const response=await fetch("http://localhost:3000/api/doctor1");
         // https://healthdebug.vercel.app/api/login
 
-        // const response=await fetch("https://healthdebug.vercel.app/api/doctor1");
+        const response=await fetch("https://healthdebug.vercel.app/api/doctor1");
         // const response=await fetch("http://localhost:3000/api/users");
         const data=await response.json();
         return data;
