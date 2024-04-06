44b64aad20b063413049f1b887b7823caf309451
devRojha <devraj227804@gamil.com>
2024-04-06 20:02:18 +0530

    doctors page created and home page initiate

--- src/app/doctorsinfo/[doctorId]/page.js
+++ src/app/doctorsinfo/[doctorId]/page.js
@@ -4,30 +4,31 @@ import { MdOutlinePermPhoneMsg } from "react-icons/md";
 const page = ({params}) => {
     console.log(params.doctorId)
   return (
-    <div className="p-8 h-screen w-full bg-white border-8  border-r-blue-400 border-l-black border-b-black border-t-blue-600"> 
-      <div className="grid grid-cols-5 h-[70%]">
-        <div className="border-b border-l mr-20 border-blue-700 col-span-3">Picture</div>
+    <div className="p-8 max-md:p-0 w-full bg-white border-8  border-r-blue-400 border-l-black border-b-black border-t-blue-600"> 
+      <div className="grid grid-cols-5 max-sm:grid-cols-1 pb-8 ">
+        <div className="border-b-2 border-l-2 mr-20 max-md:mr-2 max-sm:mr-0 border-blue-700 col-span-3">
+          <img className=' h-full' src='/doc.jpg' alt='doctor loading' />
+        </div>
         {/* Profile */}
         <div className="col-span-2 pt-8 pl-2">
           <div className="font-bold text-slate-600">PROFILE</div>
           <div className="flex">
-            <div className="text-blue-500 text-2xl font-bold mr-2">Dr. First Name</div>
-            <div className="text-blue-500 text-2xl font-bold">Last Name</div>
+            <div className="text-blue-500 text-2xl font-bold mr-2">Dr. Name</div>
           </div>
           <div className="font-bold text-slate-500 mb-2 ">Specification</div>
           <div className="font-bold text-slate-500 mb-10">Affilation</div>
-          <div className="flex flex-col space-y-4">
-            <div className="grid grid-cols-4 ">
-              <div className=" font-semibold col-span-1">EXPERIENCE</div>
-              <div className="text-slate-500 font-semibold col-span-3">19 years+</div>
+          <div className="flex flex-col space-y-4 text-1xl max-md:text-sm">
+            <div className="grid grid-cols-5 max-md:grid-cols-2">
+              <div className=" font-semibold col-span-2 max-md:col-span-1">EXPERIENCE</div>
+              <div className="text-slate-500 font-semibold col-span-3 max-md:col-span-1">19 years+</div>
             </div>
-            <div className="grid grid-cols-4  w-full">
-              <div className=" font-semibold col-span-1">LANGUAGES</div>
-              <div className="text-slate-500 font-semibold col-span-3">English, Spanish</div>
+            <div className="grid grid-cols-5 max-md:grid-cols-2 w-full">
+              <div className=" font-semibold col-span-2 max-md:col-span-1">LANGUAGES</div>
+              <div className="text-slate-500 font-semibold col-span-3 max-md:col-span-1">English, Hindi</div>
             </div>
-            <div className="grid grid-cols-4  w-full">
-              <div className=" font-semibold col-span-1">TYPE OF</div>
-              <div className="text-slate-500 font-semibold col-span-3">Surgeon</div>
+            <div className="grid grid-cols-5 max-md:grid-cols-2 w-full">
+              <div className=" font-semibold col-span-2 max-md:col-span-1">TYPE OF</div>
+              <div className="text-slate-500 font-semibold col-span-3 max-md:col-span-1">Surgeon</div>
             </div>
           </div>
           <div className="mt-10">
@@ -46,33 +47,33 @@ const page = ({params}) => {
         </div>
       </div>
       {/* bottom */}
-      <div className="grid grid-cols-5 h-[30%]">
+      <div className="flex justify-between  mb-4">
         <div className="col-span-3 flex justify-start pl-8 pt-4">
-          <div className="flex flex-col">
-            <div className="flex justify-start text-3xl font-bold">DR. First Name</div>
-            <div className="flex justify-start text-3xl text-slate-600 mb-2">Second Name</div>
-            <div className="flex justify-start text-1xl text-slate-400">Affilation</div>
-            <div className="flex justify-start text-1xl text-slate-400">Hospital</div>
+          <div className="flex flex-col ">
+            <div className="flex justify-start text-3xl max-sm:text-lg font-bold">DR.</div>
+            <div className="flex justify-start text-3xl max-sm:text-lg text-slate-600 mb-2">Name</div>
+            <div className="flex justify-start text-1xl max-sm:text-sm text-slate-400">Affilation</div>
+            <div className="flex justify-start text-1xl max-sm:text-sm text-slate-400">Hospital</div>
           </div>
         </div>
-        <div className="col-span-2 flex justify-start pl-8 pt-4">
+        <div className="col-span-2 flex justify-start pl-8 pt-4 pr-4">
           <div className="flex flex-col">
-            <div className="flex justify-end text-3xl font-bold">
+            <div className="flex justify-end text-3xl max-sm:text-lg font-bold">
               <div className="flex flex-col justify-center">
                 <div className="h-8 w-8 bg-blue-500 mr-3 flex justify-center">
                   <div className="flex flex-col justify-center text-white">+</div>
                 </div>
               </div>
-              <div>Hospital</div>
+              <div className='max-sm:text-lg'>Hospital</div>
             </div>
-            <div className="flex justify-start text-2xl text-white  bg-blue-500 pl-4 pr-4 mb-2 mt-2">
+            <div className="flex justify-start text-2xl max-sm:text-sm text-white  bg-blue-500 pl-4 pr-4 mb-2 mt-2">
               <div className='flex flex-col justify-center mr-4'><MdOutlinePermPhoneMsg /></div>
               0123 456 789</div>
             <div className="flex">
               <div className="h-full w-1 bg-blue-500 rounded-full mr-2"></div>
-              <div className="flex flex-col">
-                <div className="flex justify-start text-1xl text-slate-400">Mail</div>
-                <div className="flex justify-start text-1xl text-slate-400">Your Address</div>
+              <div className="flex flex-col text-1xl max-sm:text-sm">
+                <div className="flex justify-start text-slate-400">Mail</div>
+                <div className="flex justify-start text-slate-400">Your Address</div>
               </div>
             </div>
           </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,16 @@ export const metadata={
 }
 export default function Home() {
   return (
-    <main className="pt-10">
-        <div>
-            home
+    <main className="border border-red-600 h-screen">
+        <div className={`mt-2 mb-2 border h-[90%] bg-[url('/appointmentDoc.jpg')] bg-cover`}>
+            <div className="border border-yellow-600 flex flex-col justify-center pl-48 h-full">
+              <div>We are hare for your care.</div>
+              <div>Best care &</div>
+              <div>Better Doctor.</div>
+              <div>In publishing and graphic design, Lorem ipsum (/ˌlɔː.rəm ˈɪp.səm/) is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider </div>
+              <div>hii</div>
+              <div>hii</div>
+            </div>
         </div>
     </main>
   );
